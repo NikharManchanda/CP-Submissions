5baebe0938cd3a03bd1597f1e15328525318557c
Nikhar Manchanda <nikharmanchanda40@gmail.com>
2020-09-24 22:02:00 +0530

    Add solution for problem `B - Mix Juice` Link: https://atcoder.jp/contests/abc171/tasks/abc171_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc171/submissions/16993553

---
+++ atcoder/abc171/B.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define M 1000000007
+long long int power(int a,int b)
+{
+    if(b==0)
+        return 1;
+    long long int k=power(a,b/2);
+    if(b%2==0)
+        return ((k%M)*(k%M))%M;
+    else
+        return ((k%M)*(k%M)*(a%M))%M;
+}
+long long int fact(long long int n)
+{
+    if(n==1 || n==0)
+        return 1;
+    else
+        return ((n%M)*(fact(n-1)%M))%M;
+
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+     #ifndef ONLINE_JUDGE
+ 
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+ 
+   #endif
+
+  int n,k;
+  cin>>n>>k;
+  int p[n];
+  for(int i=0;i<n;i++)
+    cin>>p[i];
+  sort(p,p+n);
+  long long int sum=0;
+  for(int i=1;i<=k;i++)
+  {
+    sum=sum+p[i-1];
+  }
+  cout<<sum;
+
+ return 0;
+}
+
+
+
+
+
+
