00c0a77f2040f9bb2004608265d6d1168ba49c84
Nikhar Manchanda <nikharmanchanda40@gmail.com>
2020-09-25 16:58:00 +0530

    Add solution for problem `B - Popular Vote` Link: https://atcoder.jp/contests/abc161/tasks/abc161_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc161/submissions/17003576

---
+++ atcoder/abc161/B.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define M 1000000007
+long long int power(int a,int b)
+{
+    if(b==0)
+        return 1;
+    long long int k=power(a,b/2);
+    if(b%2==0)
+        return ((k%M)*(k%M))%M;
+    else
+        return ((k%M)*(k%M)*(a%M))%M;
+}
+long long int fact(long long int n)
+{
+    if(n==1 || n==0)
+        return 1;
+    else
+        return ((n%M)*(fact(n-1)%M))%M;
+
+}
+int div(int n)
+{  int c=0;
+    for(int i=1;i<sqrt(n);i++)
+    {
+        if(n%i==0)
+        {
+          c++;
+        }
+    }
+    c=c*2;
+    if(pow(sqrt(n),2)==(sqrt(n)*sqrt(n)))
+        c++;
+    return  c;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+     #ifndef ONLINE_JUDGE
+ 
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+ 
+   #endif
+   
+
+   int n,m;
+   cin>>n>>m;
+   int a[n];
+   long long int sum=0;
+   for(int i=0;i<n;i++)
+   {
+
+    cin>>a[i];
+    sum=sum+a[i];
+
+   }
+
+   int c=0;
+   
+   for(int i=0;i<n;i++)
+   {
+     if(a[i]*4*m>=sum)
+        c++;
+   }
+   if(c>=m)
+    cout<<"Yes";
+else
+    cout<<"No";
+
+
+ return 0;
+}
+
+
+
+
+
+
