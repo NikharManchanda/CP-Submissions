03870d3023bcfe5815663e372287fa487cc967e2
Nikhar Manchanda <nikharmanchanda40@gmail.com>
2021-01-30 00:59:00 +0530

    Add solution for problem `A - Exams` Link: https://codeforces.com/contest/194/problem/A Tags: implementation, math, *900 Ref: https://codeforces.com/contest/194/submission/105958108

---
+++ codeforces/194/A.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mp make_pair
+#define pb push_back
+#define lim 300005
+typedef long long ll;
+typedef pair<ll, ll> ii;
+typedef vector<ll> vi;
+typedef vector<ii> vii;
+typedef deque<ll> di;
+typedef deque<ii> dii;
+long long mod = 1000000007;
+/* -----------------------     CODE BELOW    ------------------------------------*/
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+
+
+  /*
+  #ifndef ONLINE_JUDGE
+
+    freopen("Ip-A.txt", "r", stdin);
+    freopen("Op-A.txt", "w", stdout);
+
+  #endif
+  */
+
+  int n, k;
+  cin >> n >> k;
+  int ans = 3 * n;
+  if (k >= 3 * n)
+  {
+    cout << 0;
+  }
+  else
+  {
+    cout << 3 * n - k;
+  }
+
+
+
+
+
+  return 0;
+
+}
