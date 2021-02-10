46622a123376b5bbc4807cd8808ad9e600438083
Nikhar Manchanda <nikharmanchanda40@gmail.com>
2021-02-11 02:02:00 +0530

    Add solution for problem `A - Irrational problem` Link: https://codeforces.com/contest/68/problem/A Tags: implementation, number theory, *1100 Ref: https://codeforces.com/contest/68/submission/107088390

---
+++ codeforces/68/A.cpp
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mp make_pair
+#define pb push_back
+#define lim 100006
+typedef long long ll;
+typedef pair<ll, ll> ii;
+typedef vector<ll> vi;
+typedef vector<ii> vii;
+typedef deque<ll> di;
+typedef deque<ii> dii;
+long long mod = 1000000007;
+const double pi = acos(-1.0);
+/*------------------------------------------------------------------*/
+
+long long binpow(long long a, long long b, long long m) {
+  a %= m;
+  long long res = 1;
+  while (b > 0) {
+    if (b & 1)
+      res = res * a % m;
+    a = a * a % m;
+    b >>= 1;
+  }
+  return res;
+}
+
+ll C(ll n,   ll k)
+{
+  ll res = 1;
+
+  // Since C(n, k) = C(n, n-k)
+  if (k > n - k)
+    k = n - k;
+
+  // Calculate value of
+  // [n * (n-1) *---* (n-k+1)] / [k * (k-1) *----* 1]
+  for (ll i = 0; i < k; ++i) {
+    res *= (n - i);
+    res /= (i + 1);
+  }
+
+  return res;
+}
+
+ll gcd(ll a, ll b , ll &x , ll &y)
+{
+  if (a == 0)
+  {
+    x = 0;
+    y = 1;
+    return b;
+  }
+
+  ll x1, y1;
+  ll d = gcd(b % a, a, x1, y1);
+  y = x1;
+  x = y1 - (b / a) * x1;
+  return d;
+}
+
+vector<ll> pfactors(ll n)
+{
+  vector<ll> R;
+  for (ll i = 2; i * i <= n; i++)
+  {
+    if (n % i == 0)
+    {
+      while (n % i == 0)
+      {
+        R.pb(i);
+        n = n / i;
+      }
+    }
+  }
+  if (n > 1)R.pb(n);
+  return R;
+}
+/*------------------------------------------------------------------*/
+
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+
+
+  /*
+  #ifndef ONLINE_JUDGE
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+  #endif
+  */
+
+  int p[4], a, b;
+  for (int i = 0; i < 4; i++)
+    cin >> p[i];
+  cin >> a >> b;
+  int ans = 0;
+  for (int i = a; i <= b; i++)
+  {
+    int s = 0;
+    int n = 4;
+    sort(p, p + n);
+    do
+    {
+      int f = ((((i % p[0]) % p[1]) % p[2]) % p[3]);
+      if (f == i)
+        s++;
+    } while (next_permutation(p, p + n));
+    if (s >= 7)
+    {
+      ans++;
+    }
+
+  }
+  cout << ans;
+
+
+
+
+  return 0;
+
+
+}
+
+
