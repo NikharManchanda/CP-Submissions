4633de790aced3eaf05cae3239c5465b4b5a2395
Nikhar Manchanda <nikharmanchanda40@gmail.com>
2021-02-11 01:48:00 +0530

    Add solution for problem `A - k-rounding` Link: https://codeforces.com/contest/858/problem/A Tags: brute force, math, number theory, *1100 Ref: https://codeforces.com/contest/858/submission/107087972

---
+++ codeforces/858/A.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mp make_pair
+#define pb push_back
+#define lim 100006
+typedef long long ll;
+typedef pair<ll, ll> ii;
+typedef vector<ll> vi;
+typedef vector<ii> vii;
+typedef deque<ll> di;
+typedef deque<ii> dii;
+long long mod = 1000000007;
+const double pi = acos(-1.0);
+/*------------------------------------------------------------------*/
+
+long long binpow(long long a, long long b, long long m) {
+  a %= m;
+  long long res = 1;
+  while (b > 0) {
+    if (b & 1)
+      res = res * a % m;
+    a = a * a % m;
+    b >>= 1;
+  }
+  return res;
+}
+
+ll C(ll n,   ll k)
+{
+  ll res = 1;
+
+  // Since C(n, k) = C(n, n-k)
+  if (k > n - k)
+    k = n - k;
+
+  // Calculate value of
+  // [n * (n-1) *---* (n-k+1)] / [k * (k-1) *----* 1]
+  for (ll i = 0; i < k; ++i) {
+    res *= (n - i);
+    res /= (i + 1);
+  }
+
+  return res;
+}
+
+ll gcd(ll a, ll b , ll &x , ll &y)
+{
+  if (a == 0)
+  {
+    x = 0;
+    y = 1;
+    return b;
+  }
+
+  ll x1, y1;
+  ll d = gcd(b % a, a, x1, y1);
+  y = x1;
+  x = y1 - (b / a) * x1;
+  return d;
+}
+
+vector<ll> pfactors(ll n)
+{
+  vector<ll> R;
+  for (ll i = 2; i * i <= n; i++)
+  {
+    if (n % i == 0)
+    {
+      while (n % i == 0)
+      {
+        R.pb(i);
+        n = n / i;
+      }
+    }
+  }
+  if (n > 1)R.pb(n);
+  return R;
+}
+/*------------------------------------------------------------------*/
+
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+
+
+  /*
+  #ifndef ONLINE_JUDGE
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+  #endif
+  */
+
+  long long int n, k;
+  cin >> n >> k;
+  long long int t = n;
+  long long int h = n;
+  long long int a = 0, b = 0;
+  while (t % 5 == 0)
+  {
+    t = t / 5;
+    a++;
+  }
+  while (h % 2 == 0)
+  {
+    h = h / 2;
+    b++;
+  }
+  ll x = 1, y = 1;
+  for (long long int i = 1; i <= k - a; i++)
+  {
+    n = n * 5;
+  }
+  for (long long int i = 1; i <= k - b; i++)
+  {
+    n = n * 2;
+  }
+
+  cout << n;
+
+
+
+
+  return 0;
+
+
+}
+
+
