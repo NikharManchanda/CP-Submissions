3dbd74c7a6fa1bfde6d434904f3d85d8dbcfb9b5
Nikhar Manchanda <nikharmanchanda40@gmail.com>
2021-02-11 14:40:00 +0530

    Add solution for problem `A - XXXXX` Link: https://codeforces.com/contest/1364/problem/A Tags: brute force, data structures, number theory, two pointers, *1200 Ref: https://codeforces.com/contest/1364/submission/107112527

---
+++ codeforces/1364/A.cpp
@@ -0,0 +1,180 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mp make_pair
+#define pb push_back
+#define lim 100006
+typedef long long ll;
+typedef pair<ll, ll> ii;
+typedef vector<ll> vi;
+typedef vector<ii> vii;
+typedef deque<ll> di;
+typedef deque<ii> dii;
+long long mod = 1000000007;
+const double pi = acos(-1.0);
+/*------------------------------------------------------------------*/
+
+long long binpow(long long a, long long b, long long m) {
+  a %= m;
+  long long res = 1;
+  while (b > 0) {
+    if (b & 1)
+      res = res * a % m;
+    a = a * a % m;
+    b >>= 1;
+  }
+  return res;
+}
+
+ll C(ll n,   ll k)
+{
+  ll res = 1;
+
+  // Since C(n, k) = C(n, n-k)
+  if (k > n - k)
+    k = n - k;
+
+  // Calculate value of
+  // [n * (n-1) *---* (n-k+1)] / [k * (k-1) *----* 1]
+  for (ll i = 0; i < k; ++i) {
+    res *= (n - i);
+    res /= (i + 1);
+  }
+
+  return res;
+}
+
+ll gcd(ll a, ll b , ll &x , ll &y)
+{
+  if (a == 0)
+  {
+    x = 0;
+    y = 1;
+    return b;
+  }
+
+  ll x1, y1;
+  ll d = gcd(b % a, a, x1, y1);
+  y = x1;
+  x = y1 - (b / a) * x1;
+  return d;
+}
+
+vector<ll> pfactors(ll n)
+{
+  vector<ll> R;
+  for (ll i = 2; i * i <= n; i++)
+  {
+    if (n % i == 0)
+    {
+      while (n % i == 0)
+      {
+        R.pb(i);
+        n = n / i;
+      }
+    }
+  }
+  if (n > 1)R.pb(n);
+  return R;
+}
+
+bool isPrime(ll n)
+{
+  // Corner cases
+  if (n <= 1)
+    return false;
+  if (n <= 3)
+    return true;
+
+  // This is checked so that we can skip
+  // middle five numbers in below loop
+  if (n % 2 == 0 || n % 3 == 0)
+    return false;
+
+  for (ll i = 5; i * i <= n; i = i + 6)
+    if (n % i == 0 || n % (i + 2) == 0)
+      return false;
+
+  return true;
+}
+/*------------------------------------------------------------------*/
+
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+
+
+  /*
+  #ifndef ONLINE_JUDGE
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+  #endif
+  */
+
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n, k;
+    cin >> n >> k;
+    int a[n + 2];
+    a[0] = 0;
+    a[n + 1] = 0;
+    for (int i = 1; i < n + 1; i++)
+      cin >> a[i];
+    int p[n + 1];
+    p[0] = 0;
+    for (int i = 1; i <= n; i++)
+    {
+      p[i] = p[i - 1] + a[i];
+      // cout << p[i] << endl;
+    }
+    int ans = 0;
+    for (int i = 0; i <= n - 1; i++)
+    {
+      if ((p[n] - p[i]) % k != 0)
+      {
+        ans = n - i ;
+        break;
+      }
+    }
+    int x = 0;
+    int s[n + 1];
+    s[n] = 0;
+    for (int i = n - 1 ; i >= 0; i--)
+    {
+      s[i] = s[i + 1] + a[i + 1];
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+      if ((s[0] - s[i]) % k  != 0)
+      {
+        x = i ;
+      }
+    }
+    if (ans == 0 && x == 0)
+    {
+      cout << -1;
+    }
+    else
+    {
+
+      cout << max(x, ans);
+    }
+    cout << endl;
+
+  }
+
+
+
+  return 0;
+
+
+}
+
+
