dd23deb0960a33b9cb81d7106559abfb30c4f002
Nikhar Manchanda <nikharmanchanda40@gmail.com>
2021-02-16 21:21:00 +0530

    Add solution for problem `D - Permutation Transformation` Link: https://codeforces.com/contest/1490/problem/D Tags: dfs and similar, divide and conquer, implementation, *1200 Ref: https://codeforces.com/contest/1490/submission/107589894

---
+++ codeforces/1490/D.cpp
@@ -0,0 +1,184 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+//#define mp make_pair
+#define pb push_back
+#define lim 100006
+#define all(con) con.begin(),con.end()
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+        tree_order_statistics_node_update> is;
+//Indexed Set(is)
+//(idx or we can say order)
+//Declaration -> is p;
+//Returns iterator to the element at idx i -->  p.find_by_order(i);
+//Returns the Value at idx i               --> *p.find_by_order(i);
+//Returns the Index of number x            -->  p.order_of_key(x) ;
+typedef long long ll;
+typedef pair<ll, ll> ii;
+typedef vector<ll> vi;
+typedef vector<ii> vii;
+typedef deque<ll> di;
+typedef deque<ii> dii;
+long long mod = 1000000007;
+const double pi = acos(-1.0);
+/*------------------------------------------------------------------*/
+
+long long binpow(long long a, long long b, long long m) {
+  a %= m;
+  long long res = 1;
+  while (b > 0) {
+    if (b & 1)
+      res = res * a % m;
+    a = a * a % m;
+    b >>= 1;
+  }
+  return res;
+}
+
+ll C(ll n,   ll k)
+{
+  ll res = 1;
+
+  // Since C(n, k) = C(n, n-k)
+  if (k > n - k)
+    k = n - k;
+
+  // Calculate value of
+  // [n * (n-1) *---* (n-k+1)] / [k * (k-1) *----* 1]
+  for (ll i = 0; i < k; ++i) {
+    res *= (n - i);
+    res /= (i + 1);
+  }
+
+  return res;
+}
+
+ll gcd(ll a, ll b , ll &x , ll &y)
+{
+  if (a == 0)
+  {
+    x = 0;
+    y = 1;
+    return b;
+  }
+
+  ll x1, y1;
+  ll d = gcd(b % a, a, x1, y1);
+  y = x1;
+  x = y1 - (b / a) * x1;
+  return d;
+}
+
+void pfactors(ll n , vector<ll> &R)
+{
+
+  for (ll i = 2; i * i <= n; i++)
+  {
+    if (n % i == 0)
+    {
+      while (n % i == 0)
+      {
+        R.pb(i);
+        n = n / i;
+      }
+    }
+  }
+  if (n > 1)R.pb(n);
+
+  return ;
+}
+
+bool isPrime(ll n)
+{
+  // Corner cases
+  if (n <= 1)
+    return false;
+  if (n <= 3)
+    return true;
+
+  // This is checked so that we can skip
+  // middle five numbers in below loop
+  if (n % 2 == 0 || n % 3 == 0)
+    return false;
+
+  for (ll i = 5; i * i <= n; i = i + 6)
+    if (n % i == 0 || n % (i + 2) == 0)
+      return false;
+
+  return true;
+}
+/*------------------------------------------------------------------*/
+int d[101];
+int a[101];
+void fun(int s, int e, int val)
+{
+  if (s > e)
+    return ;
+
+
+  int cmax = INT_MIN;
+  int idx = 0;
+  for (int i = s; i <= e; i++)
+  {
+    if (cmax < a[i])
+    {
+      cmax = a[i];
+      idx = i;
+    }
+  }
+  d[cmax] = val;
+  fun(s, idx - 1, val + 1);
+  fun(idx + 1, e, val + 1);
+}
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+
+
+  /*
+  #ifndef ONLINE_JUDGE
+
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+  #endif
+  */
+
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+      cin >> a[i];
+    }
+    d[n] = 0;
+    int c = 0;
+    fun(0, n - 1, c);
+    for (int i = 0; i < n; i++)
+      cout << d[a[i]] << " ";
+
+    cout << endl;
+
+
+
+
+
+  }
+
+
+
+
+
+
+
+  return 0;
+
+
+}
