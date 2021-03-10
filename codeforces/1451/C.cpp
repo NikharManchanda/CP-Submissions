c977a748119dc0e6c09c90e79ce9ced5cfc7a9f4
Nikhar Manchanda <nikharmanchanda40@gmail.com>
2021-03-10 13:02:00 +0530

    Add solution for problem `C - String Equality` Link: https://codeforces.com/contest/1451/problem/C Tags: dp, greedy, hashing, implementation, strings, *1400 Ref: https://codeforces.com/contest/1451/submission/109539242

--- codeforces/1451/C.cpp
+++ codeforces/1451/C.cpp
@@ -1,83 +1,134 @@
-/******************************************************************************************************************************
-                                     ██████╗░░█████╗░██╗░░██╗██╗░░██╗██╗███╗░░██╗
-                                     ██╔══██╗██╔══██╗██║░░██║██║░██╔╝██║████╗░██║
-                                     ██████╔╝███████║███████║█████═╝░██║██╔██╗██║
-                                     ██╔══██╗██╔══██║██╔══██║██╔═██╗░██║██║╚████║
-                                     ██║░░██║██║░░██║██║░░██║██║░╚██╗██║██║░╚███║
-                                     ╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝╚═╝░░╚══╝
-*******************************************************************************************************************************/
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <sstream>
-#include <queue>
-#include <deque>
-#include <bitset>
-#include <iterator>
-#include <list>
-#include <stack>
-#include <map>
-#include <set>
-#include <functional>
-#include <numeric>
-#include <utility>
-#include <climits>
-#include <time.h>
-#include <math.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
 using namespace std;
-/*******  All Required define Pre-Processors and typedef Constants *******/
-#define sll(t) scanf("%d",&t)
-#define slll(t) scanf("%ld",&t)
-#define sllll(t) scanf("%lld",&t)
-#define sch(t) scanf("%c",&t)
-#define sstr(t) scanf("%s",t)
-#define sf(t) scanf("%f",&t)
-#define slf(t) scanf("%lf",&t)
-#define mem(a, b) memset(a, (b), sizeof(a))
-#define FOR(i, j, k, in) for (ll i=j ; i<k ; i+=in)
-#define RFOR(i, j, k, in) for (ll i=j ; i>=k ; i-=in)
-#define REP(i, j) FOR(i, 0, j, 1)
-#define RREP(i, j) RFOR(i, j, 0, 1)
-#define all(cont) cont.begin(), cont.end()
-#define rall(cont) cont.end(), cont.begin()
-#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
-#define nl "\n"
+#define endl "\n"
 #define mp make_pair
 #define pb push_back
-#define INF (ll)1e9
-#define EPS 1e-9
-#define mod 1000000007
-const double pi = acos(-1.0);
-typedef long long ll;
-typedef pair<ll, ll> pii;
+#define lim 100008
+#define all(con) con.begin(),con.end()
+#define rall(con) con.rbegin(),con.rend()
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+        tree_order_statistics_node_update> is;
+//Indexed Set(is)
+//(idx or we can say order)
+//Declaration -> is p;
+//Returns iterator to the element at idx i -->  p.find_by_order(i);
+//Returns the Value at idx i               --> *p.find_by_order(i);
+//Returns the Index of number x            -->  p.order_of_key(x) ;
+typedef long long ll ;
+typedef pair<ll, ll> ii;
 typedef vector<ll> vi;
-typedef vector<string> vs;
-typedef vector<pii> vii;
-typedef vector<vi> vvi;
-typedef map<ll, ll> mpii;
-typedef set<ll> seti;
-typedef multiset<ll> mseti;
-/*******  Input streaming function for usaco *******/
-void setIO(string s)
+typedef vector<ii> vii;
+typedef map<ll, ll> mi;
+typedef deque<ll> di;
+typedef deque<ii> dii;
+long long mod = 1000000007;
+const double pi = acos(-1.0);
+/*------------------------------------------------------------------*/
+
+long long binpow(long long a, long long b, long long m) {
+  a %= m;
+  long long res = 1;
+  while (b > 0) {
+    if (b & 1)
+      res = res * a % m;
+    a = a * a % m;
+    b >>= 1;
+  }
+  return res;
+}
+
+ll C(ll n,   ll k)
 {
-  freopen((s + ".in").c_str(), "r", stdin);
-  freopen((s + ".out").c_str(), "w", stdout);
+  ll res = 1;
+
+  // Since C(n, k) = C(n, n-k)
+  if (k > n - k)
+    k = n - k;
+
+  // Calculate value of
+  // [n * (n-1) *---* (n-k+1)] / [k * (k-1) *----* 1]
+  for (ll i = 0; i < k; ++i) {
+    res *= (n - i);
+    res /= (i + 1);
+  }
+
+  return res;
+}
+
+ll gcd(ll a, ll b , ll &x , ll &y)
+{
+  if (a == 0)
+  {
+    x = 0;
+    y = 1;
+    return b;
+  }
+
+  ll x1, y1;
+  ll d = gcd(b % a, a, x1, y1);
+  y = x1;
+  x = y1 - (b / a) * x1;
+  return d;
+}
+
+void pfactors(ll n , vector<ll> &R)
+{
+
+  for (ll i = 2; i * i <= n; i++)
+  {
+    if (n % i == 0)
+    {
+      while (n % i == 0)
+      {
+        R.pb(i);
+        n = n / i;
+      }
+    }
+  }
+  if (n > 1)R.pb(n);
+
+  return ;
+}
+
+
+bool isPrime(ll n)
+{
+  // Corner cases
+  if (n <= 1)
+    return false;
+  if (n <= 3)
+    return true;
+
+  // This is checked so that we can skip
+  // middle five numbers in below loop
+  if (n % 2 == 0 || n % 3 == 0)
+    return false;
+
+  for (ll i = 5; i * i <= n; i = i + 6)
+    if (n % i == 0 || n % (i + 2) == 0)
+      return false;
+
+  return true;
 }
 /*------------------------------------------------------------------*/
-signed main()
+
+int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
-// #ifndef ONLINE_JUDGE
 
-//   setIO("gymnastics");
-// #endif
+
+
+  // #ifndef ONLINE_JUDGE
+
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+
+  // #endif
+
 
   int t;
   cin >> t;
@@ -101,5 +152,6 @@ signed main()
 
 
   return 0;
+
 
 }
