a14afc9e3193ed4bb07d6a2e2fd9f74a4ec2868e
Nikhar Manchanda <nikharmanchanda40@gmail.com>
2021-06-26 17:36:00 +0530

    Add solution for problem `B - Hydrate` Link: https://atcoder.jp/contests/abc207/tasks/abc207_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc207/submissions/23762365

---
+++ atcoder/abc207/B.cpp
@@ -0,0 +1,74 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define int               long long
+#define nl                "\n"
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define all(x)            (x).begin(),(x).end()
+#define rall(x)           (x).end(),(x).begin()
+#define uniq(v)           (v).erase(unique(all(v)),(v).end())
+#define sz(x)             (int)((x).size())
+#define fr                first
+#define sc                second
+#define pii               pair<int,int>
+#define rep(i,a,b)        for(int i=a;i<b;i++)
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define fix(prec)         {cout << setprecision(prec) << fixed;}
+#define lcm(a, b)         ((a) * (b)) / __gcd(a, b)
+#define rev               greater<int>()
+#define Max(x,y,z)        max(x,max(y,z))
+#define Min(x,y,z)        min(x,min(y,z))
+const double pi = acos(-1.0);
+const long long INF = 1e18;
+const int32_t M = 1e8;
+const int32_t MM = 998244353;
+
+const int N = 1e5 + 5;
+
+int dx[8] = {1, 0, -1, 0, 1, 1, -1, -1};
+int dy[8] = {0, 1, 0, -1, -1, 1, 1, -1};
+
+
+
+template<typename T, typename T1>T amax(T &a, T1 b) {if (b > a)a = b; return a;}
+template<typename T, typename T1>T amin(T &a, T1 b) {if (b < a)a = b; return a;}
+
+
+void setIO(string s)
+{
+  freopen((s + ".in").c_str(), "r", stdin);
+  freopen((s + ".out").c_str(), "w", stdout);
+}
+/*--------------------------------------------------------------------------------------------------*/
+
+
+signed main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  // setIO("swap");
+
+
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  if (d * c > b)
+  {
+    int x = a / ((d * c) - b);
+    if (a % (d * c - b) != 0)
+      x++;
+    cout << x;
+  }
+  else
+    cout << -1;
+
+
+  return 0;
+
+}
+
+
