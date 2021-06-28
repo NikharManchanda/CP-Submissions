46162c2fb42e903bedb0b1f2028e2feb1108ac20
Nikhar Manchanda <nikharmanchanda40@gmail.com>
2021-06-28 18:13:00 +0530

    Add solution for problem `D - Santa's Bot` Link: https://codeforces.com/contest/1279/problem/D Tags: combinatorics, math, probabilities, *1700 Ref: https://codeforces.com/contest/1279/submission/120801807

---
+++ codeforces/1279/D.cpp
@@ -0,0 +1,114 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define int               long long
+#define nl                "\n"
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define all(x)            (x).begin(),(x).end()
+#define rall(x)           (x).end(),(x).begin()
+#define uniq(v)           (v).erase(unique(all(v)),(v).end())
+#define sz(x)             (int)((x).size())
+#define fr                first
+#define sc                second
+#define pii               pair<int,int>
+#define rep(i,a,b)        for(int i=a;i<b;i++)
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define fix(prec)         {cout << setprecision(prec) << fixed;}
+#define lcm(a, b)         ((a) * (b)) / __gcd(a, b)
+#define rev               greater<int>()
+#define Max(x,y,z)        max(x,max(y,z))
+#define Min(x,y,z)        min(x,min(y,z))
+const double pi = acos(-1.0);
+const long long INF = 1e18;
+const int32_t M = 1e8;
+const int32_t MM = 998244353;
+
+const int N = 1e6 + 5;
+
+int dx[8] = {1, 0, -1, 0, 1, 1, -1, -1};
+int dy[8] = {0, 1, 0, -1, -1, 1, 1, -1};
+
+
+
+template<typename T, typename T1>T amax(T &a, T1 b) {if (b > a)a = b; return a;}
+template<typename T, typename T1>T amin(T &a, T1 b) {if (b < a)a = b; return a;}
+
+
+void setIO(string s)
+{
+  freopen((s + ".in").c_str(), "r", stdin);
+  freopen((s + ".out").c_str(), "w", stdout);
+}
+/*--------------------------------------------------------------------------------------------------*/
+
+int add(int a, int b) {
+  a += b;
+  if (a >= MM) a -= MM;
+  return a;
+}
+
+int mul(int a, int b) {
+  return a * b % MM;
+}
+
+int modpow(int a, int b, int m) {
+  a %= m;
+  int res = 1;
+  while (b > 0) {
+    if (b & 1)
+      res = res * a % m;
+    a = a * a % m;
+    b >>= 1;
+  }
+  return res;
+}
+
+int inv[N];
+
+signed main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  // setIO("swap");
+
+  int n;
+  cin >> n;
+  map<int, int> mp;
+  vector<int> a[n + 1];
+  for (int i = 0; i < n; i++)
+  {
+    int k;
+    cin >> k;
+    for (int j = 0; j < k; j++)
+    {
+      int x;
+      cin >> x;
+      mp[x]++;
+      a[i].pb(x);
+    }
+  }
+  int ans = 0;
+  for (int i = 0; i < N; i++)
+  {
+    inv[i] = modpow(i, MM - 2, MM);
+  }
+  for (int i = 0; i < n; i++)
+  {
+    for (auto x : a[i])
+    {
+      ans = add(ans, mul(mul(inv[n], inv[a[i].size()]), mul(mp[x], inv[n])));
+    }
+  }
+  cout << ans;
+
+
+  return 0;
+
+}
+
+
