7a2c9a6829f4de7484ba219db115445518c8c6a5
Nikhar Manchanda <nikharmanchanda40@gmail.com>
2021-08-03 04:09:00 +0530

    Add solution for problem `A - Candy Distribution Again` Link: https://atcoder.jp/contests/agc027/tasks/agc027_a Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/agc027/submissions/24742751

---
+++ atcoder/agc027/A.cpp
@@ -0,0 +1,163 @@
+    #include "bits/stdc++.h"
+    using namespace std;
+
+    /*--------------------------------------------------------------------------------------------------*/
+
+    void __print(int x) {cerr << x;}
+    void __print(long x) {cerr << x;}
+    void __print(long long x) {cerr << x;}
+    void __print(unsigned x) {cerr << x;}
+    void __print(unsigned long x) {cerr << x;}
+    void __print(unsigned long long x) {cerr << x;}
+    void __print(float x) {cerr << x;}
+    void __print(double x) {cerr << x;}
+    void __print(long double x) {cerr << x;}
+    void __print(char x) {cerr << '\'' << x << '\'';}
+    void __print(const char *x) {cerr << '\"' << x << '\"';}
+    void __print(const string &x) {cerr << '\"' << x << '\"';}
+    void __print(bool x) {cerr << (x ? "true" : "false");}
+
+    template<typename T, typename V>
+    void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+    template<typename T>
+    void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i : x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+    void _print() {cerr << "]\n";}
+    template <typename T, typename... V>
+    void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+    #ifndef ONLINE_JUDGE
+    #define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+    #else
+    #define debug(x...)
+    #endif
+
+    /*--------------------------------------------------------------------------------------------------*/
+
+    /* unordered_map<int , int, custom_hash> mp; */
+
+    struct custom_hash {
+        static uint64_t splitmix64(uint64_t x) {
+            x += 0x9e3779b97f4a7c15;
+            x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+            x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+            return x ^ (x >> 31);
+        }
+
+        size_t operator()(uint64_t x) const {
+            static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+            return splitmix64(x + FIXED_RANDOM);
+        }
+    };
+
+    /*--------------------------------------------------------------------------------------------------*/
+    #define int               long long
+    #define nl                "\n"
+    #define pb                push_back
+    #define ppb               pop_back
+    #define pf                push_front
+    #define ppf               pop_front
+    #define all(x)            (x).begin(),(x).end()
+    #define rall(x)           (x).rbegin(),(x).rend()
+    #define uniq(v)           (v).erase(unique(all(v)),(v).end())
+    #define sz(x)             (int)((x).size())
+    #define fr                first
+    #define sc                second
+    #define pii               pair<int,int>
+    #define rep(i,a,b)        for(int i=a;i<b;i++)
+    #define mem1(a)           memset(a,-1,sizeof(a))
+    #define mem0(a)           memset(a,0,sizeof(a))
+    #define fix(prec)         {cout << setprecision(prec) << fixed;}
+    #define lcm(a, b)         ((a) * (b)) / __gcd(a, b)
+    #define rev               greater<int>()
+    #define Max(x,y,z)        max(x,max(y,z))
+    #define Min(x,y,z)        min(x,min(y,z))
+    #define imin              INT_MIN
+    #define imax              INT_MAX
+    #define Yes               cout<<"Yes\n"
+    #define No                cout<<"No\n"
+    #define YES               cout<<"YES\n"
+    #define NO                cout<<"NO\n"
+    #define yes               cout<<"yes\n"
+    #define no                cout<<"no\n"
+    #define show(A) for (auto i: A) cout << i << " "; cout << '\n';
+    #define endl "\n"
+
+    using ld = long double;
+    using vi = vector < int > ;
+    using mi = map < int, int > ;
+    using pi = pair < int, int > ;
+
+    const double Pi = acos(-1.0);
+    const int inf = 1e18 + 1;
+    const int M = 1e9 + 7;
+    const int MM = 998244353;
+
+    const int N = 1e5 + 5;
+
+    int dx[8] = {1, 0, -1, 0, 1, 1, -1, -1};
+    int dy[8] = {0, 1, 0, -1, -1, 1, 1, -1};
+
+    template<typename T, typename T1>T amax(T &a, T1 b) {if (b > a)a = b; return a;}
+    template<typename T, typename T1>T amin(T &a, T1 b) {if (b < a)a = b; return a;}
+    /*--------------------------------------------------------------------------------------------------*/
+    void init()
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        //setIO("swap");
+        //local();
+    }
+    void setIO(string s)
+    {
+        freopen((s + ".in").c_str(), "r", stdin);
+        freopen((s + ".out").c_str(), "w", stdout);
+    }
+    void local()
+    {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    }
+    /*--------------------------------------------------------------------------------------------------*/
+
+    signed main()
+    {
+        init();
+
+        int n, x;
+        cin >> n >> x;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        sort(a, a + n);
+        int c = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (i == n - 1)
+            {
+                if (x == a[i])
+                    c++;
+                else
+                    break;
+            }
+            else
+            {
+                if (x - a[i] >= 0)
+                {
+                    c++;
+                    x = x - a[i];
+                }
+                else
+                    break;
+            }
+
+        }
+
+        cout << c;
+
+
+        return 0;
+
+    }
